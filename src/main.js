05ca77f5c9432c04ce30df8b9c12806831178774
CodeMaster7000 <CodeMaster7K@gmail.com>
2024-08-03 22:59:40 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,42 +5,41 @@ var one = new ProgressBar.Circle('#one', {
 			duration: 1000,
 			easing: 'linear'
 		});
-		var two = new ProgressBar.Circle('#two', {
+var two = new ProgressBar.Circle('#two', {
 			color: '#f4c300',
 			trailColor: 'transparent',
 			strokeWidth: 9,
 			duration: 1000,
 			easing: 'linear'
 		});
-		var three = new ProgressBar.Circle('#three', {
+var three = new ProgressBar.Circle('#three', {
 			color: '#020200',
 			trailColor: 'transparent',
 			strokeWidth: 9,
 			duration: 1000,
 			easing: 'linear'
 		});
-		var four = new ProgressBar.Circle('#four', {
+var four = new ProgressBar.Circle('#four', {
 			color: '#009f3d',
 			trailColor: 'transparent',
 			strokeWidth: 9,
 			duration: 1000,
 			easing: 'linear'
 		});
-		var five = new ProgressBar.Circle('#five', {
+var five = new ProgressBar.Circle('#five', {
 			color: '#df0024',
 			trailColor: 'transparent',
 			strokeWidth: 9,
 			duration: 1000,
 			easing: 'linear'
 		});
-		one.animate(1.0, function() {
-			two.animate(1.0, function() {
-				three.animate(1.0, function() {
-					four.animate(1.0, function() {
-						five.animate(1.0, function() {
-
-						});
-					});
+one.animate(1.0, function() {
+	two.animate(1.0, function() {
+		three.animate(1.0, function() {
+			four.animate(1.0, function() {
+				five.animate(1.0, function() {
 				});
 			});
 		});
+	});
+});
